ac34295d262bd820880d3a57e89c5d4078b7ec0c
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2021-01-02 18:32:52 +0900

    外部キー無しの設定追加

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -2,5 +2,5 @@ class Partner < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to:decision
 
-  belongs_to :customer
+  belongs_to :customer, optional: true
 end
